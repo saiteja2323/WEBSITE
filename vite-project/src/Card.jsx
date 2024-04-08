3aae1b06d56f2bf474eb155096e638c053d8d6fb
Pavan Kalyan <69674111+pavan416@users.noreply.github.com>
2024-04-08 11:55:41 +0530

    button fixed

---
+++ vite-project/src/Card.jsx
@@ -0,0 +1,15 @@
+function Card() {
+  return (
+    <div className="card">
+      <img
+        className="card-image"
+        src="http://via.placeholder.com/150"
+        alt="Profile picture"
+      />
+      <h2 className="card-tittle">Bro Code</h2>
+      <p className="card-text">I make youtube videos and play video games</p>
+    </div>
+  );
+}
+
+export default Card;
---
+++ vite-project/src/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; {new Date().getFullYear()} our Website Name</p>
+    </footer>
+  );
+}
+export default Footer;
